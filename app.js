02b1f52e9813fd90d26a11d7c369d8f4fb6a7db3
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-07-03 13:50:59 +1200

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+function movieHandler(req, res) {
+    let question = req.query.question;
+    res.send(`Hello, ${question}`);
+}
+
+app.get('/movie', movieHandler);
+
+/**
+ * "/movie?question=wherexx"
+ * */
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
